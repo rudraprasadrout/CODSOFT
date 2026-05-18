9cac6c2cb3bdda4e607c269892b7ea4f3a317fb9
Rudra Prasad Rout <rudraprasadrout07@gmail.com>
2026-05-18 10:49:08 +0530

    Update app.py

--- Task_3_Sale_Prediction/app.py
+++ Task_3_Sale_Prediction/app.py
@@ -85,4 +85,5 @@ def predict():
         return jsonify({"prediction": predictions[0]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
